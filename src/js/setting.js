1af750a050fe1934e4422c628ce30bc7bb6c2bc4
nahcoos <hscsc515@gmail.com>
2017-09-08 14:25:04 +0900

    update jquery mask plugin

--- src/js/setting.js
+++ src/js/setting.js
@@ -1,5 +1,6 @@
 require('bootstrap');
 require('../less/setting.less');
+require('jquery-mask-plugin');
 
 var common = require('./common');
 var tab = require('./ht-tab');
@@ -30,12 +31,34 @@ function init(member) {
     $('.ht-setting-email').html(member.email);
     $('#ht-member-name-input').val(member.detail.name);
     $('#ht-member-phone-input').val(member.detail.phone);
+    $('#ht-member-phone-input').mask('000-****-0000');
 
     if (member.detail.info === 'Y') {
         $('#ht-member-info-check').attr('checked', true);
     }
 
     if (!member.detail.avatar) {
-        $('.ht-setting-avatar-img').css('background-img', 'url('+ member.detail.avatar +')');
+        $('.ht-setting-avatar-img').css('background-image', 'url('+ member.detail.avatar +')');
     }
+
+    $('#ht-setting-avatar-select').on('click', function () {
+        $('#ht-setting-avatar-input').click();
+    });
+
+    $('#ht-setting-avatar-input').on('change', function () {
+        if (this.files.length === 0) {
+            return;
+        }
+
+        var file = this.files[0];
+        var fileReader = new FileReader();
+
+        fileReader.addEventListener('load', function (event) {
+            var preview = event.target.result;
+
+            $('.ht-setting-avatar-img').css('background-image', 'url('+ preview +')');
+        });
+
+        fileReader.readAsDataURL(file);
+    });
 }
