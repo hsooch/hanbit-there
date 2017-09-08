8b4c05a5a72b3660f56af90f67cb61133740dbec
nahcoos <hscsc515@gmail.com>
2017-09-08 11:36:02 +0900

    update getMemberDetail

--- src/js/setting.js
+++ src/js/setting.js
@@ -12,5 +12,30 @@ common.ajax({
             alert('로그인이 필요한 페이지입니다.');
             location.href = '/';
         }
+
+        getMemberDetail();
     }
 });
+
+function getMemberDetail() {
+    common.ajax({
+        url: '/api/member/detail',
+        success: function (result) {
+            init(result);
+        }
+    });
+}
+
+function init(member) {
+    $('.ht-setting-email').html(member.email);
+    $('#ht-member-name-input').val(member.detail.name);
+    $('#ht-member-phone-input').val(member.detail.phone);
+
+    if (member.detail.info === 'Y') {
+        $('#ht-member-info-check').attr('checked', true);
+    }
+
+    if (!member.detail.avatar) {
+        $('.ht-setting-avatar-img').css('background-img', 'url('+ member.detail.avatar +')');
+    }
+}
