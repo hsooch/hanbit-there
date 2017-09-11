29ff042c0bb7beb1841f83a73d8eed8f135102ea
nahcoos <hscsc515@gmail.com>
2017-09-11 12:58:55 +0900

    update saveMemberDetail, avatar image

--- src/js/setting.js
+++ src/js/setting.js
@@ -31,14 +31,14 @@ function init(member) {
     $('.ht-setting-email').html(member.email);
     $('#ht-member-name-input').val(member.detail.name);
     $('#ht-member-phone-input').val(member.detail.phone);
-    $('#ht-member-phone-input').mask('000-****-0000');
+    $('#ht-member-phone-input').mask('000-0000-0000');
 
     if (member.detail.info === 'Y') {
         $('#ht-member-info-check').attr('checked', true);
     }
 
-    if (!member.detail.avatar) {
-        $('.ht-setting-avatar-img').css('background-image', 'url('+ member.detail.avatar +')');
+    if (member.detail.avatar) {
+        $('.ht-setting-avatar-img').css('background-image', 'url(' + member.detail.avatar + ')');
     }
 
     $('#ht-setting-avatar-select').on('click', function () {
@@ -56,9 +56,63 @@ function init(member) {
         fileReader.addEventListener('load', function (event) {
             var preview = event.target.result;
 
-            $('.ht-setting-avatar-img').css('background-image', 'url('+ preview +')');
+            $('.ht-setting-avatar-img').css('background-image', 'url(' + preview + ')');
         });
 
         fileReader.readAsDataURL(file);
     });
 }
+
+$('.ht-setting-save').on('click', function () {
+    var member = {
+        currentPw: $('#ht-member-cpw-input').val().trim(),
+        password: $('#ht-member-npw-input').val().trim(),
+        detail: {
+            name: $('#ht-member-name-input').val().trim(),
+            phone: $('#ht-member-phone-input').cleanVal().trim(),   // mask 함수. - 나 , 를 빼고 db 에 저장
+            info: $('#ht-member-info-check')[0].checked ? 'Y' : 'N'
+        }
+    };
+
+    var npwc = $('#ht-member-npwc-input').val().trim();
+
+    if (member.currentPw || member.password || npwc) {
+        if (!member.currentPw) {
+            alert('현재 비밀번호를 입력하세요.');
+            $('#ht-member-cpw-input').focus();
+            return;
+        }
+        else if (!member.password) {
+            alert('새로운 비밀번호를 입력하세요.');
+            $('#ht-member-npw-input').focus();
+            return;
+        }
+        else if (member.password !== npwc) {
+            alert('새 비밀번호를 확인하세요.');
+            $('#ht-member-npwc-input').focus();
+            return;
+        }
+    }
+
+    var formData = new FormData();
+    formData.append('member', JSON.stringify(member));
+
+    var images = $('#ht-setting-avatar-input')[0].files;
+    if (images.length > 0) {
+        formData.append('avatar', images[0]);
+    }
+
+    common.ajax({
+        url: '/api/member/save',
+        method: 'POST',
+        data: formData,
+        contentType: false,
+        processData: false,
+        success: function (result) {
+            alert('정상적으로 수정되었습니다.');
+
+            location.reload();
+        }
+    });
+
+});
